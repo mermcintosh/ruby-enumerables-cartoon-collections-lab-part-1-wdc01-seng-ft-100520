d0a22cc9ff5fc0c87e999d33b2c4f09b9f9bda9d
Meredith McIntosh <meredith@mcintosh.io>
2020-09-22 18:48:33 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,11 +1,22 @@
 def greet_characters(array)
-  # Use `each` to enumerate over the provided array
-  #
-  # Print a custom greeting for each element
+  array.each do |character_names|
+    puts "Hello #{character_names}!"
+  end
 end
+greet_characters(["Hoggle", "Ludo", "Sir Didymus"])
 
 def list_dwarves(array)
-  # Use `each_with_index` to enumerate over the provided array
-  #
-  # Print a numbered list of each element
+  array.each_with_index do |dwarf_names, index|
+    puts "#{index + 1}. #{dwarf_names}"
+  end
 end
+list_dwarves(["Bashful", "Doc", "Dopey", "Grumpy", "Happy", "Sleepy", "Sneezy"])
+
+# def roll_call_dwarves(dwarves)
+#   dwarves.map.with_index do |dwarf, i|
+#     "#{i + 1}. #{dwarf}"
+#   end.join("\n")
+# end
+
+# dwarves = %w{Doc Dopey Bashful Grumpy}
+# puts roll_call_dwarves(dwarves)
